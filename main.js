3b3f651f41de54e84ce0a1596f84a4eb12f0fcbd
Christoph Reiter <reiter.christoph@gmail.com>
2026-06-04 21:30:24 +0200

    wip

--- main.js
+++ main.js
@@ -138,6 +138,7 @@ async function downloadInstaller() {
   // We use the last field only, so that each version is ensured semver incompatible with the previous one.
   const version = `0.0.${INSTALLER_VERSION.replace(/-/g, '')}`
   const inst_path = tc.find('msys2-installer', version, 'x64');
+  throw new Error('Installer caching is currently disabled'); // see
   const destination = inst_path ? path.join(inst_path, 'base.exe') : await tc.downloadTool("https://httpbin.org/response-headers?Content-Length=99999");
   let computedChecksum = await computeChecksum(destination);
   if (computedChecksum.toUpperCase() !== INSTALLER_CHECKSUM.toUpperCase()) {
