2d117bacb20e2b03abfc4524d899b43ff8f7cacf
Christoph Reiter <reiter.christoph@gmail.com>
2026-06-04 21:26:43 +0200

    wip

--- main.js
+++ main.js
@@ -138,7 +138,7 @@ async function downloadInstaller() {
   // We use the last field only, so that each version is ensured semver incompatible with the previous one.
   const version = `0.0.${INSTALLER_VERSION.replace(/-/g, '')}`
   const inst_path = tc.find('msys2-installer', version, 'x64');
-  const destination = inst_path ? path.join(inst_path, 'base.exe') : await tc.downloadTool("https://expired.badssl.com/");
+  const destination = inst_path ? path.join(inst_path, 'base.exe') : await tc.downloadTool("https://httpbin.org/response-headers?Content-Length=99999");
   let computedChecksum = await computeChecksum(destination);
   if (computedChecksum.toUpperCase() !== INSTALLER_CHECKSUM.toUpperCase()) {
     throw new Error(`The SHA256 of the installer does not match! expected ${INSTALLER_CHECKSUM} got ${computedChecksum}`);
